96de774eddb18695b29ab7b3f655929a8f68695e
mikelghedina <mikel.ghedina@gmail.com>
2020-10-19 13:15:38 +0200

    new exercise P12 exercise updates.

---
+++ src/main/java/Practica12/Pet.java
@@ -0,0 +1,60 @@
+package Practica12;
+
+import java.util.ArrayList;
+
+public class Pet {
+    private String petType;
+    private ArrayList<Vaccine> vaccines;
+    private double weight;
+    private double height;
+
+
+    public Pet(String petType, ArrayList<Vaccine> vaccines, double weight, double height) {
+        this.petType = petType;
+        this.vaccines = vaccines;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "petType='" + petType + '\'' +
+                ", vaccines=" + vaccines +
+                ", weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+    public ArrayList<Vaccine> getVaccines() {
+        return vaccines;
+    }
+
+    public void setVaccines(ArrayList<Vaccine> vaccines) {
+        this.vaccines = vaccines;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
---
+++ src/main/java/Practica12/Vaccine.java
@@ -0,0 +1,32 @@
+package Practica12;
+
+public class Vaccine {
+    private String vaccineName;
+    private boolean isVaccinated;
+    private Date vaccineDate;
+
+
+    public String getVaccineName() {
+        return vaccineName;
+    }
+
+    public void setVaccineName(String vaccineName) {
+        this.vaccineName = vaccineName;
+    }
+
+    public boolean isVaccinated() {
+        return isVaccinated;
+    }
+
+    public void setVaccinated(boolean vaccinated) {
+        isVaccinated = vaccinated;
+    }
+
+    public Date getVaccineDate() {
+        return vaccineDate;
+    }
+
+    public void setVaccineDate(Date vaccineDate) {
+        this.vaccineDate = vaccineDate;
+    }
+}
